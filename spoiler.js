0b6cfa27df2ef49667c125ec4269a0b6018ae28f
k3499 <k3499@yandex.ru>
2023-03-06 18:09:03 +0300

    багфикс разворачивания после сварачивания

--- spoiler.js
+++ spoiler.js
@@ -2,24 +2,31 @@
 
 const spoilerLink = document.querySelector('.spoiler__link');
 const spoiler = document.querySelector('.spoiler');
+const swiperBlock = document.querySelector('.swiper');
+
 let spoilerStatus = 0;
 
 spoilerLink.addEventListener('click', (evt) =>{
     evt.preventDefault();
     
     if (spoilerStatus === 0){
-        //spoiler.style.height = 'auto';
-        spoiler.classList.remove('fadeOut');
-        spoiler.classList.add('fadeIn');
+        swiperBlock.style.display = 'none';
+        spoiler.style.display = 'block';
+        setTimeout(() => {
+            spoiler.classList.remove('fadeOut');
+            spoiler.classList.add('fadeIn');
+        }, 50);
+
         spoiler.style.height = 'fit-content';
         spoilerStatus = 1;
     }else if(spoilerStatus === 1){
-        //spoiler.style.display = 'none';
+        setTimeout(() => {swiperBlock.style.display = 'block'}, 550);
+        setTimeout(() => {spoiler.style.display = 'none'}, 550);
         spoilerStatus = 0;
         spoiler.classList.remove('fadeIn');
         spoiler.classList.add('fadeOut');
     }
-    console.log(spoilerStatus)
+    console.log(spoiler)
 })
 
 var swiper = new Swiper(".swiper", {
