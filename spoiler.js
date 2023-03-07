2a0c54f1e606ccb9d04b9c9052ddee8db57e9281
k3499 <k3499@yandex.ru>
2023-03-07 11:23:43 +0300

    финальная версия с адаптивными тегами

--- spoiler.js
+++ spoiler.js
@@ -2,7 +2,8 @@
 
 const spoilerLink = document.querySelector('.spoiler__link');
 const spoiler = document.querySelector('.spoiler');
-const swiperBlock = document.querySelector('.swiper');
+const swiperBlock = document.querySelector('.slider__wrapper');
+const slider = document.querySelector('.slider');
 
 let spoilerStatus = 0;
 
@@ -10,18 +11,22 @@ spoilerLink.addEventListener('click', (evt) =>{
     evt.preventDefault();
     
     if (spoilerStatus === 0){
+        
         swiperBlock.style.display = 'none';
         spoiler.style.display = 'block';
         setTimeout(() => {
+            slider.style.height = 'auto';
             spoiler.classList.remove('fadeOut');
             spoiler.classList.add('fadeIn');
         }, 50);
-
         spoiler.style.height = 'fit-content';
         spoilerStatus = 1;
     }else if(spoilerStatus === 1){
-        setTimeout(() => {swiperBlock.style.display = 'block'}, 550);
-        setTimeout(() => {spoiler.style.display = 'none'}, 550);
+        setTimeout(() => {
+            swiperBlock.style.display = 'block'
+            spoiler.style.display = 'none'
+            slider.style.height = '33px';
+        }, 550);
         spoilerStatus = 0;
         spoiler.classList.remove('fadeIn');
         spoiler.classList.add('fadeOut');
@@ -30,7 +35,7 @@ spoilerLink.addEventListener('click', (evt) =>{
 })
 
 var swiper = new Swiper(".swiper", {
-    slidesPerView: 5,
+    slidesPerView: 'auto',
     spaceBetween: 20,
     autoHeight: true,
     navigation: {
